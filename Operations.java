3c0ea27c100dbad06ae915a34bfbebbd21ab29e7
Shubom123 <shubomnath365@gmail.com>
2022-07-02 17:52:47 +0530

    Added: All elements added in binary tree

--- Operations.java
+++ Operations.java
@@ -7,6 +7,16 @@ public class Operations {
         System.out.println("Building tree with root value " + root.key);
         tree.insert(root,30);
         tree.insert(root,70);
+        tree.insert(root,22);
+        tree.insert(root,40);
+        tree.insert(root,60);
+        tree.insert(root,95);
+        tree.insert(root,11);
+        tree.insert(root,65);
+        tree.insert(root,3);
+        tree.insert(root,16);
+        tree.insert(root,63);
+        tree.insert(root,67);
         tree.traversal(root);
     }
 }
